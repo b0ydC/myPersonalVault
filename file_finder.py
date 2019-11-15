b733a7b86b9f26fd2b9ac02c56e8de2364dae0ca
boydC <39641738+boydC506@users.noreply.github.com>
2019-11-15 00:47:42 -0600

    file_finder.py

--- file_finder.py
+++ file_finder.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+#Instructions
+#
+# To search for any extention file in the entire filesystem, just modify the pattern.
+# ex: pattern = '*.py'
+# to search for python files.
+#
+# v1
+# 11/15/2019
+
+
 import fnmatch
 import os
 
